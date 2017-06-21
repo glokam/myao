e4ef5b24368a6c2d7e14b5f5f4bacb0ea2ad0d50
Kamil Głowacki <glowacki.kam@gmail.com>
2017-06-21 21:41:13 +0200

    webpack & eslint first configuration

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+	entry: './lib/myao.js',
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{ test: /\.jsx?$/,
+			  include: [
+  		  		path.resolve(__dirname, "lib")
+			  ],
+			  loader: 'eslint-loader',
+			  exclude: /node_modules/ }
+		]
+	}
+}
